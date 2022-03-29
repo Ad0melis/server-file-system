63ee4d99cf43f4ebbe9138c65a00174754bdd55f
Ad0melis <96545771+Ad0melis@users.noreply.github.com>
2022-03-29 17:57:04 +0300

    Users update

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
 import { file } from './lib/file.js';
 
-const status = await file.create('users', 'petras.json', false);
-console.log(status);
-file.read();
-file.update();
-file.read();
-file.delete();
-file.read();
+// const createFileStatus1 = await file.create('users', 'petras.json', {
+//     name: 'Petras',
+//     age: 99,
+//     isMarried: true
+// });
+// console.log(createFileStatus1);
+
+// const createFileStatus2 = await file.create('users', 'maryte.json', {
+//     name: 'Maryte',
+//     age: 87,
+//     isMarried: false
+// });
+// console.log(createFileStatus2);
+
+const listStatus = await file.list('users');
+console.log(listStatus);
+
+// const readFileStatus1 = await file.read('users', 'petras.json');
+// console.log(readFileStatus1);
+
+// const readFileStatus2 = await file.read('users', 'maryte.json');
+// console.log(readFileStatus2);
+
+// const updateFileStatus = await file.update('users', 'petras.json', {
+//     name: 'Petras',
+//     age: 99
+// });
+// console.log(updateFileStatus);
+
+// const readFileStatus2 = await file.read('users', 'petras.json');
+// console.log(readFileStatus2);
+
+// const updateFileStatus2 = await file.update('users', 'petras.json', {
+//     name: 'Petras'
+// });
+// console.log(updateFileStatus2);
+
+// const readFileStatus3 = await file.read('users', 'petras.json');
+// console.log(readFileStatus3);
+
+// const deleteFileStatus = await file.delete('users', 'petras.json');
+// console.log(deleteFileStatus);
+
+// const readFileStatus4 = await file.read('users', 'petras.json');
+// console.log(readFileStatus4);
---
+++ users.js
@@ -0,0 +1,17 @@
+import { file } from './lib/file.js';
+// const users = ['Petras', 'Maryte', 'Jonas', 'Ona'];
+
+const users = [
+    { name: 'Petras', age: 99, isMarried: true },
+    { name: 'Maryte', age: 87, isMarried: false },
+    { name: 'Jonas', age: 66, isMarried: false },
+    { name: 'Ona', age: 54, isMarried: true },
+];
+
+
+for (const user of users) {
+   const fileName = user.name.toLocaleLowerCase()+'.json';
+   const status = await file.create('users', fileName, user)
+   console.log(fileName, status);
+}
+
